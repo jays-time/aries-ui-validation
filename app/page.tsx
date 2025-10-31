8533945b76c9034db969b2d241b7db90f42e4e32
Jayanth Nimmalapalli <jayanthkumar@Jays-MacBook-Pro.local>
2025-10-31 14:20:24 -0500

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-
-export default function Home() {
+export default async function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div>Hello World!</div>
-      </main>
-    </div>
+    <>
+      <h1 className="mb-8 text-5xl font-bold">Hello World!</h1>
+    </>
   );
 }
